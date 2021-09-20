a0b943e3e79547745ba7001371606e5b3c218527
Devin Power <dpwr16@gmail.com>
2021-09-19 22:56:01 -0500

    Added BidData class

--- weightedavg.py
+++ weightedavg.py
@@ -3,6 +3,8 @@ from build.db import Connection
 from abc import ABC
 
 class TransportList():
+    '''Columns of the transport list from the Items table.'''
+
     df: pd.DataFrame
     query: str = '''
     SELECT Item.ItemID, Item.Description, Item.Unit
@@ -12,8 +14,39 @@ class TransportList():
         with Connection() as conn:
             self.df = pd.read_sql(self.query, conn, index_col='ItemID')
 
+class BidData():
+    '''Bid data with functions for serving filtered dataframes.'''
+
+    df: pd.DataFrame
+    query: str = '''
+    SELECT 
+    Contract.Year, Contract.District, Contract.County, Bid.ContractID, 
+    Bid.ItemID, Item.Description, Item.Unit, Bid.Quantity, 
+    Bid.Engineer_UnitPrice, Bid.Engineer_TotalPrice, 
+    Bid.BidderID_0_UnitPrice, Bid.BidderID_0_TotalPrice, 
+    Bid.BidderID_1_UnitPrice, Bid.BidderID_1_TotalPrice,
+    Bid.BidderID_2_UnitPrice, Bid.BidderID_2_TotalPrice
+
+    FROM Bid 
+
+    JOIN Contract ON Bid.ContractID = Contract.ContractID,
+    Item ON Bid.ItemID = Item.ItemID
+
+    WHERE Contract.Year >= 2018'''
+
+    def __init__(self) -> None:
+        with Connection() as conn:
+            self.df = pd.read_sql(self.query, conn)
+
+    def get_df_by_year(self, year: int):
+        '''Returns a dataframe filtered by year.'''
+
+        filt = self.df['Year'] == year
+        return self.df[filt].copy()
+
 class WeightedAverage():
     '''Base class for calculating and exporting weighted average bid prices.'''
+
     df_in: pd.DataFrame
     df_out: pd.DataFrame
     years = [2021, 2020, 2019, 2018]
@@ -95,6 +128,10 @@ class WeightedAverage():
     def to_csv(self, filename: str):
         self.df_out.to_csv(filename)
 
-wt_avg = WeightedAverage()
-print(wt_avg.df_out.info())
+# wt_avg = WeightedAverage()
+# print(wt_avg.df_out.info())
 # wt_avg.to_csv('weighted_avg.csv')
+
+# bid_data = BidData()
+# bid_data_2021 = bid_data.get_df_by_year(2021)
+# print(bid_data_2021.info())
