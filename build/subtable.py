1c5482900367b2ffd54ce76952d0244b37b4bd90
Devin Power <dpwr16@gmail.com>
2021-10-09 17:55:18 -0500

    Added implementation TODOs to ContractTable

--- build/subtable.py
+++ build/subtable.py
@@ -4,18 +4,15 @@ import pandas as pd
 from io import StringIO
 
 
-class ContractTable:
+class ContractTable: # Relies on data from the bidder table in the previous implementation
     '''Transforms raw contract subtable data into a format that can be inserted 
     into the Contract SQL table.'''
 
     def __init__(self, bid_ab: BidAbstract) -> None:
         self.table: str = 'Contract'
         self.bid_ab = bid_ab
-        # create a input_df from the contract_bytestr
         self.input_df = self.get_input_df()
-        # create a output_df formatted for insertion
         self.output_df = self.get_output_df()
-        # push to database
 
     def get_input_df(self):
         return pd.read_csv(StringIO(self.bid_ab.contract_bytestr))
@@ -24,6 +21,10 @@ class ContractTable:
         columns = get_table_columns(self.table)
         output_df = pd.DataFrame(columns=columns)
         return output_df
+
+    # TODO: Fill output_df with content
+
+    # TODO: implement to_db function
 
 bid_ab = BidAbstract(200131)
 contract_table = ContractTable(bid_ab=bid_ab)
