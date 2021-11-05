e9cf27ddedd2dc839d11a724e39e53d21e2476a4
Devin Power <dpwr16@gmail.com>
2021-11-05 14:07:32 -0500

    split scraper script into class and simpler script

--- build/scraper.py
+++ build/scraper.py
@@ -1,79 +1,61 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import Select
-from build.db import Cursor
-
-'''
-Scrapes MnDOT's abstract hosting page for all abstract download links
-for a given year.
-Appends a database with new entries from the web scrapping.
-'''
-
-# Abstract hosting webpage
-URL = 'http://transport.dot.state.mn.us/PostLetting/Abstract.aspx'
-
-# Set to False to launch a browser window
-HEADLESS = True
+from typing import List
 
 
-def main(year: str = '2021'):
-
-    print(f'Scraping {year}: Started')
-    #
-    # Initialize browser
-    #
-    browser = None
-    if HEADLESS == True:
-        opts = Options()
-        opts.headless = True
-        browser = Firefox(options=opts)
-    else:
-        browser = Firefox()
-
-    browser.get(URL)
-
-    #
-    # Navigate to all results for the target year and extract Contract IDs
-    #
-    # Select letting year
-    element = browser.find_element_by_id('MainContent_drpLettingYear')
-    select = Select(element)
-    select.select_by_value(year)
-
-    # Submit initial form
-    element = browser.find_element_by_id('MainContent_btnByLettingYear')
-    element.click()
-
-    # Set 'Records Per Page' to 'All'
-    element = browser.find_element_by_id('MainContent_drpPage')
-    select = Select(element)
-    select.select_by_value('20000')
-
-    # Get links to download csvfrom HTML table
-    element = browser.find_element_by_id('MainContent_gvabstractMenu')
-    all_rows = element.find_elements_by_tag_name('a')
-
-    # Extract contract IDs from the download links
-    contract_ids = list()
-    for row in all_rows:
-        link = row.get_attribute('href')
-        if 'ContractId' in link:
-            contract_ids.append(link[-6:])
-
-    #
-    # Add to database
-    #
-    with Cursor() as cur:
-        int_year = int(year)
-        processed = 0  # Initialize processed as 0 (False)
-        for id in contract_ids:
-            cur.execute('''INSERT OR IGNORE INTO Abstract
-            ( AbstractID, Year, Processed ) VALUES ( ?, ?, ? )''',
-            (id, int_year, processed))
-
-    browser.close()
-    print(f'Scraping {year}: Complete.')
+class AbstractIDScraper:
+    '''Retrieves a list of abstract IDs from MnDOT's hosting site using selenium webdriver.'''
+    def __init__(self, year: int = 2021, headless: bool = True) -> None:
+        self.year = year
+        self.headless = headless
+        self.url = 'http://transport.dot.state.mn.us/PostLetting/Abstract.aspx'
+        self.browser = self.get_browser()
+        self.abstract_ids: List[int] = self.get_abstract_id_list()
 
 
-if __name__ == '__main__':
-    main()
+    def get_browser(self):
+        if self.headless == True:
+            opts = Options()
+            opts.headless = True
+            browser = Firefox(options=opts)
+        else:
+            browser = Firefox()
+        browser.get(self.url)
+        return browser
+
+
+    def get_abstract_id_list(self):
+        #
+        # Navigate to all results for the target year and extract Contract IDs
+        #
+        # Select letting year
+        element = self.browser.find_element_by_id('MainContent_drpLettingYear')
+        select = Select(element)
+        select.select_by_value(str(self.year))
+
+        # Submit initial form
+        element = self.browser.find_element_by_id('MainContent_btnByLettingYear')
+        element.click()
+
+        # Set 'Records Per Page' to 'All'
+        element = self.browser.find_element_by_id('MainContent_drpPage')
+        select = Select(element)
+        select.select_by_value('20000')
+
+        # Get links to download csvfrom HTML table
+        element = self.browser.find_element_by_id('MainContent_gvabstractMenu')
+        all_rows = element.find_elements_by_tag_name('a')
+
+        # Extract contract IDs from the download links
+        contract_ids = list()
+        for row in all_rows:
+            link = row.get_attribute('href')
+            if 'ContractId' in link:
+                contract_ids.append(int(link[-6:]))
+
+        return contract_ids
+
+
+    def __del__(self):
+        self.browser.close()
---
+++ scrape_abstract_ids.py
@@ -0,0 +1,55 @@
+from sqlalchemy import select
+from build.scraper import AbstractIDScraper
+from data.model import Abstract, Session
+
+
+def get_abstract_ids_from_db(session: Session):
+    '''Retrieves a list of all Abstract IDs from the Abstract database table.'''
+    statement = select(Abstract.AbstractID)
+    result = session.execute(statement)
+
+    db_abstract_ids = []
+    for id in result.scalars():
+        db_abstract_ids.append(id)
+
+    return db_abstract_ids
+
+
+def get_new_abstract_objs(scraper: AbstractIDScraper, db_abstract_ids: list):
+    '''Generates list of new Abstract table objects to be inserted into the database.'''
+    abstract_objs = []
+
+    for scrapped_id in scraper.abstract_ids:
+        if scrapped_id not in db_abstract_ids:
+            ab_obj = Abstract(
+                    AbstractID=scrapped_id,
+                    Year=scraper.year,
+                    Processed='NO'
+                )
+            abstract_objs.append(ab_obj)
+
+    return abstract_objs
+
+
+def main():
+    with Session() as session:
+        # get a list of all Abstract IDs already in the database
+        db_abstract_ids = get_abstract_ids_from_db(session)
+
+        scraper = AbstractIDScraper(year=2021)
+
+        # compare scraped ids to list from the database
+        new_abstract_objs = get_new_abstract_objs(session, scraper, db_abstract_ids)
+
+        # Insert any new abstracts into the database
+        for obj in new_abstract_objs:
+            session.add(obj)
+
+        session.commit()
+
+    print("Scraping complete.")
+    print(f'Inserted {len(new_abstract_objs)} new Abstract IDs into the database.')
+
+
+if __name__ == '__main__':
+    main()
